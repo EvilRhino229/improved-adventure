d671dbe3ce85711339a59686f137258c2815c47d
user <user@users-MacBook-Pro-4.local>
2019-04-09 21:00:08 -0500

    Add tasks

---
+++ app/assets/javascripts/basics.js
@@ -0,0 +1,123 @@
+// alert("According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to lift its fat little body off the ground. The bee, of course, flies anyway, because bees don't care what humans think is impossible.");
+
+// don't DO this
+console.log("According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to lift its fat little body off the ground. The bee, of course, flies anyway, because bees don't care what humans think is impossible.");
+
+// console.log("2" + 2);
+
+// console.log("2" * 2);
+
+// būleans
+// console.log(true);
+// console.log(false);
+// console.log(!0);
+
+// strict equality operator
+// console.log(2 === 2);
+// console.log(2 === "2");
+
+// leß strict
+// console.log(2 == "2");
+
+// variabuuuul | "var"
+// var myNumber = 5;
+// myNumber = 4;
+// if (myNumber + 2 === 5) {
+//   console.log("Yo, 5!");
+// }  else if (myNumber === 4) {
+//   console.log("It's 4!")
+// } else {
+//   console.log("Yikes, idk man");
+// }
+
+// if (2 === 3) {
+//   console.log("ìqual");
+// } else {
+//   console.log("ūps");
+// } 
+
+// function sayHi() {
+//   console.log("sup");
+// }
+
+// dost thou hoist? functions do
+// sayHi();
+
+// function numberMult(firstNum, secondNum) {
+//   return firstNum * secondNum;
+// }
+
+// console.log(numberMult(256, 256));
+
+var myArray = [56, 12, 27, 4, 3, 10];
+// console.log(myArray);
+
+
+// brother may i have some lööp
+// counter = 0;
+// while (counter <= 10) {
+//   console.log(counter);
+//   counter++;
+// }
+
+// for (var i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+myArray = [56, 12, 27, 4, 3, 10]
+// for (var i = 0; i < myArray.length; i++) {
+//   console.log(myArray[i]);
+// }
+
+// auto-generated
+
+// for (var i = myArray.length - 1; i >= 0; i--) {
+//   console.log(myArray[i])
+// }
+
+// Objects
+
+// var person = {firstName: "Todd", lastName: "Howard"}
+// console.log(person);
+
+// 
+// console.log(person["firstName"]);
+// person.age = 47;
+// console.log(person.lastName);
+// console.log(person);
+
+// function rollDie() {
+//   return Math.floor(Math.random() * 6) +1;
+//   }
+
+// console.log(rollDie());
+
+// function fizzbuzz() {
+//   for (var i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("Fizzbuzz!")
+//     } else if (i % 3 === 0) {
+//       console.log("Fizz!")
+//     } else if (i % 5 === 0) {
+//       console.log("Buzz!")
+//     } else {
+//       console.log(i)
+//     }
+//   }
+// }
+
+// fizzbuzz();
+
+// function isPalindrome(string) {
+//   var stringReverse = string.toLowerCase().split("").filter(char => char !== " ").reverse().join("");
+//   string = string.split("").filter(char => char !== " ").join("");
+  
+//   if (stringReverse === string.toLowerCase()) {
+//     console.log("That's a palindrome!")
+//   } else {
+//     console.log("No dice (see above method)")
+//   }
+// }
+
+// isPalindrome("Not A Palindrome");
+// isPalindrome("A Man A Plan A Canal Panama");
--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,87 +1,27 @@
-// alert("According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to lift its fat little body off the ground. The bee, of course, flies anyway, because bees don't care what humans think is impossible.");
+window.addEventListener("load", function() {
+  console.log(window)
+// Document Object Model
+console.log(document);
 
-// don't DO this
-console.log("According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to lift its fat little body off the ground. The bee, of course, flies anyway, because bees don't care what humans think is impossible.");
+var heading = document.getElementById("my-heading");
+  console.log(heading);
+// var headings = document.getElementsByTagName("h1");
+var tasks = document.getElementsByClassName("task");
+  console.log(tasks);
 
-// console.log("2" + 2);
-
-// console.log("2" * 2);
-
-// būleans
-// console.log(true);
-// console.log(false);
-// console.log(!0);
-
-// strict equality operator
-// console.log(2 === 2);
-// console.log(2 === "2");
-
-// leß strict
-// console.log(2 == "2");
-
-// variabuuuul | "var"
-// var myNumber = 5;
-// myNumber = 4;
-// if (myNumber + 2 === 5) {
-//   console.log("Yo, 5!");
-// }  else if (myNumber === 4) {
-//   console.log("It's 4!")
-// } else {
-//   console.log("Yikes, idk man");
-// }
-
-// if (2 === 3) {
-//   console.log("ìqual");
-// } else {
-//   console.log("ūps");
-// } 
-
-// function sayHi() {
-//   console.log("sup");
-// }
-
-// dost thou hoist? functions do
-// sayHi();
-
-// function numberMult(firstNum, secondNum) {
-//   return firstNum * secondNum;
-// }
-
-// console.log(numberMult(256, 256));
-
-var myArray = [56, 12, 27, 4, 3, 10];
-// console.log(myArray);
+  heading.innerHTML = "may i have some för lööp";
+  heading.addEventListener("click", function() {
+  heading.classList.toggle("big-blue-heading");
 
 
-// brother may i have some lööp
-// counter = 0;
-// while (counter <= 10) {
-//   console.log(counter);
-//   counter++;
-// }
+  });
 
-// for (var i = 0; i <= 10; i++) {
-//   console.log(i);
-// }
+  for (var i = 0; i < tasks.length; i++) {
+    var task = tasks[i]
+    task.addEventListener("click", function() {
+      console.log(task);
+      task.style.color = "red"
+    });
+  }
 
-myArray = [56, 12, 27, 4, 3, 10]
-// for (var i = 0; i < myArray.length; i++) {
-//   console.log(myArray[i]);
-// }
-
-// auto-generated
-
-// for (var i = myArray.length - 1; i >= 0; i--) {
-//   console.log(myArray[i])
-// }
-
-// Objects
-
-var person = {firstName: "Todd", lastName: "Howard"}
-// console.log(person);
-
-// 
-// console.log(person["firstName"]);
-person.age = 47;
-// console.log(person.lastName);
-console.log(person);
+});
